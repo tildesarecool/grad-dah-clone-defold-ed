4667539fd122e23d828b9804c4d8778ea2976a9c
Tildes <tildesare.cool@gmail.com>
2021-12-26 19:26:30 -0800

    Made it through very basic video 2 of default tutorial, relatively painless.

---
+++ Tutorial 2 - Image Processing/build/default/main/main_generated_3.go
@@ -0,0 +1,31 @@
+components {
+  id: "wheelBounce"
+  component: "/main/wheelBounce.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/assets.atlas\"\ndefault_animation: \"wheel\"\nmaterial: \"/builtins/materials/sprite.material\"\nblend_mode: BLEND_MODE_ALPHA\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
